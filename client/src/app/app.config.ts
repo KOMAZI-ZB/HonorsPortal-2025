5109bfc356f1288b4c55b43fa4e88c37ba25752b
Zintle Komazi <komazi.dev@gmail.com>
2025-04-08 15:01:39 +0200

    Commit4 08/04/2025

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+
     provideRouter(routes),
-    provideHttpClient() //Telling angular hey i  want to use the internet
+    provideHttpClient(), //Telling angular hey i  want to use the internet
+    provideAnimations()
+
   ]
 };
