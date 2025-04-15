837e33d57a119080a2d178daa7dc19903f22893c
Zintle Komazi <komazi.dev@gmail.com>
2025-04-15 16:17:36 +0200

    Commit6 15/04/2025

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,14 +3,16 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { errorInterceptor } from './_interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
 
     provideRouter(routes),
     provideHttpClient(), //Telling angular hey i  want to use the internet
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right'
